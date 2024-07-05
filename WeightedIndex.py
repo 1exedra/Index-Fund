71c01d6b52660f0d0dbc79415635c8e08221cc26
1exedra <exedreye@gmail.com>
2024-07-05 20:03:56 +0530

    Update WeightedIndex.py
    Calculating the new weighted index with daily returns

--- WeightedIndex.py
+++ WeightedIndex.py
@@ -2,3 +2,12 @@ url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 table = pd.read_html(url)
 sp500 = table[0]
 tickers = sp500['Symbol'].tolist()
+
+start_date = '2020-01-01'
+end_date = '2024-01-01'
+
+data = yf.download(tickers, start=start_date, end=end_date)['Adj Close']
+data.dropna(axis=1, inplace=True)
+returns = data.pct_change()
+equal_weighted_returns = returns.mean(axis=1)
+equal_weighted_index = (1 + equal_weighted_returns).cumprod()
